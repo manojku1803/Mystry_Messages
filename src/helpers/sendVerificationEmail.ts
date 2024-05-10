d0ec41553dfed26c17c16e4a68d1a1e5d9381348
Manoj Kumar Pradhan <89033216+manojku1803@users.noreply.github.com>
2024-05-10 20:18:29 +0530

    Update sendVerificationEmail.ts

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,8 +9,8 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: email,
+      from: 'Acme <onboarding@resend.dev>',
+      to: [email],
       subject: 'Mystery Message Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
     });
