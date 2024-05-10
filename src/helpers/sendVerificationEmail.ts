47d391dad5d3529428a781c89223464f7c3e48a7
Manoj <manojpradhan1803@gmail.com>
2024-05-10 09:03:02 +0530

    Add the changes

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: 'ironman15870@gmail.com',
+      from: 'manojkumar1803.com',
       to: email,
       subject: 'Mystery Message Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
